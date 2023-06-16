905630f804e005281245016ba3ad20dcac6ed825
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-16 12:25:21 +0200

    setting up space to create links

---
+++ js-jsnacks-blocco-3/vue3-header/src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import NavLink from './NavLink.vue';
+
+export default {
+    components: {
+        NavLink,
+    },
+
+    data(){
+        links: [
+            "Products",
+            "Pippi",
+            "Giustino",
+            "San Pio da Pietralcina",
+            "Er Pupone"
+        ]
+    },
+
+    name: "AppHeader",    
+}
+</script>
+
+<style lang="">
+    
+</style>
---
+++ js-jsnacks-blocco-3/vue3-header/src/components/NavLink.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <a href=""></a>
+    </div>
+</template>
+<script>
+export default {
+    name: "NavLink",
+
+    data(){
+        
+    }
+}
+</script>
+<style lang="">
+    
+</style>
